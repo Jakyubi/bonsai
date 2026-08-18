45a6bce1921f548e8b59cbd678113b7ff4f9b3b6
Jakyubi <frostwth.xd@gmail.com>
2026-08-18 20:57:05 +0200

    build: configure html entry points

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, "index.html"),
+        about: resolve(import.meta.dirname, "about.html"),
+        product: resolve(import.meta.dirname, "product.html"),
+      },
+    },
+  },
 });
